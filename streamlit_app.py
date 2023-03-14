220e6b2927d4a895b60c39d01bfadd3b618e9405
hebn001 <126286229+hebn001@users.noreply.github.com>
2023-03-14 16:01:28 -0400

    Update streamlit_app.py
    normalize the fruityvice json data and displays it on the app as a dataframe table

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+# Normalize the data from a json file format
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Disaplays fruityvice info as a table
+streamlit.dataframe(fruityvice_normalized)
